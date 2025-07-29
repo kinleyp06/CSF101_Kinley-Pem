f5c8897f4c7d6a9dab2ad5bc374a88f511ccc331
kinleyp06 <kinleypem06@gmail.com>
2025-07-29 14:34:20 +0600

    First commit

---
+++ second_program.py
@@ -0,0 +1,8 @@
+a=2
+b=5
+sum=a+b
+print("The sum of a and b is: ", sum)
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Your name is ", name, "and you are ", age, "years old.")
