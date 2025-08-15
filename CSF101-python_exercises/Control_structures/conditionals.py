c63317c249403628b243af34c39d966e0cc4b939
kinleyp06 <kinleypem06@gmail.com>
2025-08-15 12:04:16 +0600

    Practical1

--- CSF101-python_exercises/Control_structures/conditionals.py
+++ CSF101-python_exercises/Control_structures/conditionals.py
@@ -1,5 +1,5 @@
 number = 10
-if number >0:
+if number > 0:
     print("The number is positive.")
 else :
     print("the number is non-positive.")
@@ -24,3 +24,26 @@ elif score >= 60:
 else:
     grade = "F"
 print(f"your grade is: {grade}")
+
+number = 7
+result = "even" if number % 2 == 0 else "odd"
+print(f"The number is {result}.")
+
+num1 = 10
+num2 = 5
+operator = "+"
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2 
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+   result = num1 / num2 if num2 != 0 else "Error: Division by zero"
+else: 
+    result = "Error: Invalid operator" 
+
+print(f"Result: {result}")
+
+
