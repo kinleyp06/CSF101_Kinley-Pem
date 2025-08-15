8c75d4d295f4cf9660eff688b8ddf00a12bef07c
kinleyp06 <kinleypem06@gmail.com>
2025-08-15 11:22:41 +0600

    Practical1

--- CSF101-python_exercises{PRACTICAL1}/Control_structures/conditionals.py
+++ CSF101-python_exercises{PRACTICAL1}/Control_structures/conditionals.py
@@ -12,4 +12,15 @@ elif number < 0:
 else: 
     print("The number is zero.")
 
-score = 87
+score = 85
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+print(f"your grade is: {grade}")
